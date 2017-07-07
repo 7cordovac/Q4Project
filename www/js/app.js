f5f92bd0f63f269bb95c4f33d3077a1e79dfb0df
Carol Cordova <carolcordova7@gmail.com>
2017-07-07 15:46:41 -0700

    nav bar footer added with icons

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic','starter.controllers','starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -25,61 +25,43 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.track', {
-      url: '/track',
+    .state('tab.home', {
+      url: "/home",
+      abstract: true,
+      templateUrl: "templates/tab-home.html"
+    })
+    .state('tab.login', {
+      url: "/login",
       views: {
-        'tab-track': {
-          templateUrl: 'templates/tab-track.html',
-          controller: 'TrackCtrl'
+        'login-tab': {
+          templateUrl: "templates/tab-login.html",
+          controller: 'LoginCtrl'
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
+    .state('tab.track', {
+      url: "/track",
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        'track-tab': {
+          templateUrl: "templates/tab-track.html"
+        }
+      }
+    })
+    .state('tab.trails', {
+      url: "/trails",
+      views: {
+        'trails-tab': {
+          templateUrl: "templates/tab-trails.html"
         }
       }
     })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+   $urlRouterProvider.otherwise("/tab");
 
+})
+
+.controller('TabsCtrl', function($scope) {
+  console.log('HomeTabCtrl');
 });
